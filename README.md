23864c7051d0387d6829804d1181739f0b1dde49
Divide-By-0 <aayushg@mit.edu>
2022-09-28 01:11:17 -0400

    first modal call, waiting on checkpoints file for the most part

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tcvc-modal
+
 Running image colorization on video in the cloud
+
+## Install
+
+```
+git clone --recurse-submodules --remote-submodules git@github.com:Divide-By-0/tcvc-modal.git
+pip install --force-reinstall https://modal.com/api/client-library/us-szqX2y2eZXtjoc3loHEdiv/modal-0.0.33-py3-none-any.whl
+# add modal api token here, dm Aayush for command
+mkdir videos
+gdown "https://drive.google.com/uc?id=1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc" # Get video
+mv <videonam_that_you_just_downloaded> videos
+python3 main.py
+```
+
+<!-- gdown --fuzzy https://drive.google.com/file/d/1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc/view -->
---
+++ main.py
@@ -0,0 +1,38 @@
+import modal
+import TCVC.codes.test_TCVC_onesampling_noGT.py
+
+setup_image = modal.Image.debian_slim().run_commands(
+    [
+        'ls',
+        'cd ~/project/codes/models/archs/networks/channelnorm_package/',
+        'python setup.py develop',
+        'cd ~/project/codes/models/archs/networks/correlation_package/',
+        'python setup.py develop',
+        'cd ~/project/codes/models/archs/networks/resample2d_package/',
+        'python setup.py develop',
+        'cd ~/project/codes',
+        # "gdown 'https://drive.google.com/uc?id=1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc'" # Get video
+    ],
+)
+
+stub = modal.Stub()
+
+@stub.function(mounts=[modal.Mount(local_dir="~/Documents/projects/tcvc-modal/", remote_dir="~/project"), modal.Mount(local_dir="~/Documents/projects/tcvc-modal/videos", remote_dir="/data2/yhliu/old_film")], image=setup_image.conda().conda_install(["cudatoolkit=10.2", "cudnn=7.6.5"]).pip_install(["tensorflow-gpu"]), gpu=True)
+def square(x):
+    print("This code is running on a remote worker!")
+    main()
+    return x**2
+
+if __name__ == "__main__":
+    with stub.run():
+        print("the square is", square(42))
+
+
+# def download_file(url):
+    # import gdown
+    # url = 'https: //drive.google.com/uc? id $=0$ B9P 1L--7Wd2vNm9zMTJWOGxobkU'
+    # output = '20150428_collected_images.tgz'
+    # gdown. download(url, output, quiet=False)
+    # $\mathrm{md} 5=$ 'fa837a88f0c40c513d975104edf3da17'
+    # gdown. cached_download(url, output, md5=md5, postprocess=gdown. extractall)
+
