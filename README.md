45a297fbcaea399d5c6e9b58343ec98e23032a0c
Divide-By-0 <aayushg@mit.edu>
2022-09-30 13:00:40 -0400

    added git clone to image

--- README.md
+++ README.md
@@ -8,12 +8,17 @@ Running image colorization on video in the cloud
 git clone --recurse-submodules --remote-submodules git@github.com:Divide-By-0/tcvc-modal.git
 sudo apt update && sudo apt-get install -y python3-pip
 pip install --force-reinstall https://modal.com/api/client-library/us-szqX2y2eZXtjoc3loHEdiv/modal-0.0.33-py3-none-any.whl
-pip install opencv-python
-# add modal api token here, dm Aayush for command
+wget https://tcvc.s3.amazonaws.com/TCVC_IDC.zip # Colorization backbone model
+wget https://tcvc.s3.amazonaws.com/pretrained_models.zip # Pretrained TCVC
+pip install -r TCVC/codes/requirements.txt
+conda install --file TCVC/codes/conda_requirements.txt
+# Run command to add modal api token here from modal.com setup (dm me at aayushg@mit.edu for access)
 mkdir videos
 gdown "https://drive.google.com/uc?id=1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc" # Get video
 mv <videonam_that_you_just_downloaded> videos
 python3 main.py
 ```
+
+Uploaded the TCVC files to s3://tcvc.
 
 <!-- gdown --fuzzy https://drive.google.com/file/d/1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc/view -->
--- main.py
+++ main.py
@@ -1,54 +1,40 @@
 import modal
 
-# setup_image = modal.Image.debian_slim().run_commands(
-#     [
-#         'ls',
-#         'cd ~/project/TCVC/codes/models/archs/networks/channelnorm_package/',
-#         'python setup.py develop',
-#         'cd ~/project/TCVC/codes/models/archs/networks/correlation_package/',
-#         'python setup.py develop',
-#         'cd ~/project/TCVC/codes/models/archs/networks/resample2d_package/',
-#         'python setup.py develop',
-#         'cd ~/project/TCVC/codes/',
-#         'python test_TCVC_onesampling_noGT.py',
-#         'export PYTHONPATH=~project/TCVC/codes'
-#         # "gdown 'https://drive.google.com/uc?id=1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc'" # Get video
-#     ],
-# )
-
-setup_image = modal.Image.debian_slim().run_commands(
-    [
-        'ls',
-        'cd ~/project/TCVC/codes/models/archs/networks/channelnorm_package/',
-        'python setup.py develop',
-        'cd ~/project/TCVC/codes/models/archs/networks/correlation_package/',
-        'python setup.py develop',
-        'cd ~/project/TCVC/codes/models/archs/networks/resample2d_package/',
-        'python setup.py develop',
-        'cd ~/project/TCVC/codes/',
-        'python test_TCVC_onesampling_noGT.py',
-        'export PYTHONPATH=~project/TCVC/codes'
-        # "gdown 'https://drive.google.com/uc?id=1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc'" # Get video
-    ],
-)
-
+setup_image = modal.Image.debian_slim()
 stub = modal.Stub()
+
+volume = modal.SharedVolume().persist("tcvc-repo-vol")
+CACHE_PATH = "~/Documents/projects/tcvc-modal"
 
 @stub.function(mounts=[modal.Mount(local_dir="~/Documents/projects/tcvc-modal/", remote_dir="~/project"),
                        modal.Mount(local_dir="~/Documents/projects/tcvc-modal/videos", remote_dir="/data2/yhliu/old_film")],
                image=setup_image.conda().conda_install(["cudatoolkit=10.2", "cudnn=7.6.5"])
-            #    .pip_install(["tensorflow-gpu", "asposestorage", "sdata"])
-               .pip_install_from_requirements("./TCVC/codes/requirements.txt"),
+               .pip_install_from_requirements("./TCVC/codes/requirements.txt").run_commands([
+                    'git clone --recurse-submodules --remote-submodules git@github.com:Divide-By-0/tcvc-modal.git',
+                    'cd tcvc-modal && git clone https://github.com/Divide-By-0/TCVC-Temporally-Consistent-Video-Colorization',
+                    'ls',
+                    'cd ~/project/TCVC/codes/models/archs/networks/channelnorm_package/',
+                    'python setup.py develop',
+                    'cd ~/project/TCVC/codes/models/archs/networks/correlation_package/',
+                    'python setup.py develop',
+                    'cd ~/project/TCVC/codes/models/archs/networks/resample2d_package/',
+                    'python setup.py develop',
+                    'cd ~/project/TCVC/codes/',
+                    'python test_TCVC_onesampling_noGT.py',
+                    'export PYTHONPATH=~project/TCVC/codes'
+                    # "gdown 'https://drive.google.com/uc?id=1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc'" # Get video
+                    ],
+                ),
+               shared_volumes={CACHE_PATH: volume},
                gpu=True)
-
 def square(x):
     print("This code is running on a remote worker!")
     return x**2
 
+
 if __name__ == "__main__":
     with stub.run():
         print("the square is", square(42))
-
 
 # def download_file(url):
     # import gdown
