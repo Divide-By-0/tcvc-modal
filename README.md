c9677e619b6c3602c39c5483b1514f79124afab8
Divide-By-0 <aayushg@mit.edu>
2023-03-17 20:37:00 +0700

    work majorized by cudanexus

--- README.md
+++ README.md
@@ -10,15 +10,17 @@ On your laptop:
 git clone --recursive git@github.com:Divide-By-0/tcvc-modal.git
 cd tcvc-modal && git clone https://github.com/Divide-By-0/TCVC-Temporally-Consistent-Video-Colorization
 mv TCVC-Temporally-Consistent-Video-Colorization TCVC
-cd ..
 python3 main.py
 ```
 
 Setup on the machine running the code:
+Choose an AWS Community AMI with Cuda 10.2 in the name.
 
 ```
 sudo apt update && sudo apt-get install -y python3-pip
-pip install --force-reinstall https://modal.com/api/client-library/us-szqX2y2eZXtjoc3loHEdiv/modal-0.0.33-py3-none-any.whl
+curl https://bootstrap.pypa.io/pip/3.5/get-pip.py -o get-pip.py && python3 get-pip.py
+# pip install --force-reinstall https://modal.com/api/client-library/us-szqX2y2eZXtjoc3loHEdiv/modal-0.0.33-py3-none-any.whl
+pip3 install -r requirements.txt
 wget "https://tcvc.s3.amazonaws.com/TCVC_IDC.zip" # Colorization backbone model
 wget "https://tcvc.s3.amazonaws.com/pretrained_models.zip" # Pretrained TCVC
 pip install -r TCVC/codes/requirements.txt
@@ -28,6 +30,33 @@ mkdir videos
 gdown "https://drive.google.com/uc?id=1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc" # Get video
 mv <videonam_that_you_just_downloaded> videos
 python3 main.py
+```
+
+Amazon Linux: AMI ID  ami-0d245d4b67431f496 with name nvidia-driver-440.53-cuda-10.2-v14-g4dn-AMI
+```
+sudo yum install git
+sudo yum update -y
+sudo yum install -y python3
+git clone https://github.com/Divide-By-0/tcvc-modal/
+cd tcvc-modal && git clone https://github.com/Divide-By-0/TCVC-Temporally-Consistent-Video-Colorization
+mv TCVC-Temporally-Consistent-Video-Colorization TCVC
+sudo yum –y install python3-pip
+pip3 install -r requirements.txt
+wget "https://tcvc.s3.amazonaws.com/TCVC_IDC.zip" # Colorization backbone model
+wget "https://tcvc.s3.amazonaws.com/pretrained_models.zip" # Pretrained TCVC
+pip3 install -r TCVC/codes/requirements.txt --use-feature=2020-resolver
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh
+exit
+# re-ssh
+cd tcvc-modal
+conda install --file TCVC/codes/conda_requirements.txt
+sudo yum groupinstall "Development Tools"
+wget "https://tcvc.s3.amazonaws.com/cudnn-local-repo-rhel7-8.6.0.163-1.0-1.x86_64.rpm"
+sudo yum localinstall cudnn-local-repo-rhel7-8.6.0.163-1.0-1.x86_64.rpm
+cd TCVC/codes/models/archs/networks/channelnorm_package/
+CUDA_HOME=~/miniconda3/pkgs/cuda-toolkit/ python setup.py develop
+CUDA_HOME=~/miniconda3/pkgs/cudatoolkit-10.1.243-0 python setup.py develop
 ```
 
 Uploaded the TCVC files to s3://tcvc.
--- main.py
+++ main.py
@@ -10,35 +10,52 @@ stub = modal.Stub(mounts=[modal.Mount(local_dir="~/Documents/projects/tcvc-modal
                           modal.Mount(local_dir="~/Documents/projects/tcvc-modal/videos", remote_dir="/data2/yhliu/old_film")],
                   image=setup_image.conda()
                   #    .conda_install(["cudatoolkit=10.2", "cudnn=7.6.5"])
-                  .pip_install_from_requirements("./TCVC/codes/requirements.txt")
+                  #   .pip_install_from_requirements("./requirements_pip.txt")
+                  .conda_update_from_environment(environment_yml="./environment.yml")
                   .run_commands([
                       'apt-get update',
                       'apt-get install git -y'])
                   .run_commands([
-                      'conda install -c conda-forge -y cudatoolkit=10.2 cudnn=7.6.5',
                       'git clone --recursive https://github.com/Divide-By-0/tcvc-modal',
-                  ])
-                  .run_commands([
                       'cd tcvc-modal && git clone https://github.com/Divide-By-0/TCVC-Temporally-Consistent-Video-Colorization',
-                  ])
-                  .run_commands([
                       'mv tcvc-modal/TCVC-Temporally-Consistent-Video-Colorization tcvc-modal/TCVC',
-                      'python tcvc-modal/TCVC/codes/models/archs/networks/channelnorm_package/setup.py develop',
-                      'python tcvc-modal/TCVC/codes/models/archs/networks/correlation_package/setup.py develop',
-                      'python tcvc-modal/TCVC/codes/models/archs/networks/resample2d_package/setup.py develop',
-                      'python tcvc-modal/TCVC/codes/test_TCVC_onesampling_noGT.py',
-                      #   'export PYTHONPATH=.'
+                  ])
+                  .run_commands(["find / -name '*nvcc*'"])
+                  .run_commands(['conda install anaconda && conda update --all'])
+                  .run_commands(['conda env update --name base -f /tcvc-modal/environment.yml'])
+                  .run_commands(["find / -name '*nvcc*'"])
+                  .run_commands(['apt install build-essential wget -y'])
+                  #   .run_commands(['wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run',
+                  #  ])
+                  #   .run_commands(['sh cuda_10.2.89_440.33.01_linux.run'])
+                  .run_commands([
+                      'conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch'
+                      #   'conda install -c conda-forge -y cudatoolkit=10.1 cudatoolkit-dev=10.0 cudnn=7.6.5',
+                      #   'conda install -c hcc cudatoolkit=10.1'
+                  ])
+                  .run_commands(['find / -name "*nvcc*"'])
+                  .run_commands(['find / -name "*cuda*"'])
+                  .run_commands(['nvcc --version'])
+                  .run_commands(['ls /usr/local'])
+                  .run_commands([                  # 'ls /usr/local/lib/python3.9/site-packages/cuda',
+                      'CUDA_HOME=/usr/local/lib/python3.9/site-packages/cuda python tcvc-modal/TCVC/codes/models/archs/networks/channelnorm_package/setup.py develop',
+                      'CUDA_HOME=/usr/local/lib/python3.9/site-packages/cuda python tcvc-modal/TCVC/codes/models/archs/networks/correlation_package/setup.py develop',
+                      'CUDA_HOME=/usr/local/lib/python3.9/site-packages/cuda python tcvc-modal/TCVC/codes/models/archs/networks/resample2d_package/setup.py develop',
+                      'CUDA_HOME=/usr/local/lib/python3.9/site-packages/cuda python tcvc-modal/TCVC/codes/test_TCVC_onesampling_noGT.py',
+                      # 'export PYTHONPATH=.'
                       # "gdown 'https://drive.google.com/uc?id=1876eGDhyKhc2rx2X-A_FffB3ZX2FXTOc'" # Get video
-                  ],),
-                  secret=modal.Secret.from_name("CUDA_HOME")
-                  #    shared_volumes={CACHE_PATH: volume},
+                      # ],)
+                  ])  # .run_commands(['python -c "import torch; print(torch.version.cuda)"'])
+                  ,
+                  # shared_volumes={CACHE_PATH: volume},
+                  secret=modal.Secret.from_name("CUDA_HOME"), gpu=True
                   )
 
 if __name__ == "__main__":
     with stub.run():
         print("Running")
 
-# def download_file(url):
+    # def download_file(url):
     # import gdown
     # url = 'https: //drive.google.com/uc? id $=0$ B9P 1L--7Wd2vNm9zMTJWOGxobkU'
     # output = '20150428_collected_images.tgz'
